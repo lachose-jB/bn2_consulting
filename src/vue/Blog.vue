807e4035a9bf85d075afe8e34e7fc4e38c665db1
azeto <azeto@localhost.localdomain>
2025-02-19 09:11:05 +0100

    first commit

--- src/vue/Blog.vue
+++ src/vue/Blog.vue
@@ -1,15 +1,1 @@
-<template>
-  <div>
-    <h1>Témoignages</h1>
-    <p>Voici les témoignages de nos clients satisfaits.</p>
-  </div>
-</template>
 
-<script setup lang="ts">
-</script>
-
-<style scoped>
-h1 {
-  color: #2c3e50;
-}
-</style>
