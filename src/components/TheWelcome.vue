5ad42d013d1b96e22b6e8812e82bfe0d28f32b29
azeto <azeto@localhost.localdomain>
2025-02-19 05:04:11 +0100

    first commit

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,4 +1,74 @@
 <script setup>
+import { ref } from 'vue'
+import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2'
+
+const name = ref('')
+const email = ref('')
+const subject = ref('')
+const message = ref('')
+const successMessage = ref('')
+const errorMessage = ref('')
+
+const sendEmail = async () => {
+  if (!name.value || !email.value || !subject.value || !message.value) {
+    errorMessage.value = "Veuillez remplir tous les champs.";
+    return;
+  }
+
+  const templateParams = {
+    from_name: name.value,
+    from_email: email.value,
+    subject: subject.value,
+    message: message.value
+  };
+
+  try {
+    const response = await emailjs.send(
+      'service_id',   // Remplace par ton ID de service
+      'template_id',  // Remplace par ton ID de modèle d'email
+      templateParams,
+      'user_id'       // Remplace par ton ID utilisateur
+    );
+
+    if (response.status === 200) {
+      successMessage.value = "Message envoyé avec succès !";
+      name.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value = '';
+
+      // Afficher un pop-up de succès
+      Swal.fire({
+        icon: 'success',
+        title: 'Message envoyé',
+        text: 'Votre message a bien été envoyé. Merci de nous avoir contactés !',
+        confirmButtonText: 'D\'accord'
+      });
+    } else {
+      errorMessage.value = "Une erreur est survenue. Veuillez réessayer.";
+
+      // Afficher un pop-up d'erreur
+      Swal.fire({
+        icon: 'error',
+        title: 'Erreur',
+        text: 'Une erreur est survenue lors de l\'envoi du message.',
+        confirmButtonText: 'Réessayer'
+      });
+    }
+  } catch (error) {
+    errorMessage.value = "Une erreur est survenue. Veuillez réessayer.";
+    console.error(error);
+
+    // Afficher un pop-up d'erreur
+    Swal.fire({
+      icon: 'error',
+      title: 'Erreur',
+      text: 'Une erreur est survenue lors de l\'envoi du message.',
+      confirmButtonText: 'Réessayer'
+    });
+  }
+};
 </script>
 
 <template>
@@ -301,6 +371,104 @@
     </article>
   </section>
 
+ <section class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <article class="container py-5">
+        <section class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+            <h5 class="fw-bold text-primary text-uppercase">Membres de l'équipe</h5>
+            <h1 class="mb-0">Des professionnels prêts à aider votre entreprise</h1>
+            <p>Chez BN2 Consulting, notre équipe de
+                direction est un groupe dédié de
+                professionnels passionnés, chacun
+                apportant une expertise unique pour faire
+                de notre entreprise un leader dans le
+                domaine du conseil , ingénierie
+                informatique et gestion de projet etc.
+                Nous sommes fiers de notre équipe et de
+                notre engagement envers l'excellence,
+                l'innovation et le succès continu de nos
+                clients.</p>
+        </section>
+        <section class=" owl-carousel  row g-5">
+            <article class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
+                <section class="team-item bg-light rounded overflow-hidden">
+                    <article class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="@/assets/img/team-1.jpg" alt="">
+                        <section class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </section>
+                    </article>
+                    <article class="text-center py-4">
+                        <h4 class="text-primary">MIKAEL LADIRE</h4>
+                        <p class="text-uppercase m-0">RESPONSABLE SECURITE EN CHAGE DE LA
+                            PROESPECTION COMMERCIALE</p>
+                    </article>
+                </section>
+            </article>
+
+            <article class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
+                <section class="team-item bg-light rounded overflow-hidden">
+                    <article class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="@/assets/img/team-2.jpg" alt="">
+                        <section class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </section>
+                    </article>
+                    <article class="text-center py-4">
+                        <h4 class="text-primary">NARCISSE BANDJIM</h4>
+                        <p class="text-uppercase m-0">CEO BN2 CONSULTING</p>
+                    </article>
+                </section>
+            </article>
+
+            <article class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <section class="team-item bg-light rounded overflow-hidden">
+                    <article class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="@/assets/img/team-3.jpg" alt="">
+                        <section class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </section>
+                    </article>
+                    <article class="text-center py-4">
+                        <h4 class="text-primary">DAVY JOSEPHINE</h4>
+                        <p class="text-uppercase m-0">DIRECTEUR TECHNIQUE</p>
+                    </article>
+                </section>
+            </article>
+        </section>
+
+        <section class=" owl-carousel  row g-5 mt-5">
+
+            <article class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <section class="team-item bg-light rounded overflow-hidden">
+                    <article class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="@/assets/img/photp.jpg" alt="">
+                        <section class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </section>
+                    </article>
+                    <article class="text-center py-4">
+                        <h4 class="text-primary">Jean-Baptiste ADJAHOUISSO</h4>
+                        <p class="text-uppercase m-0">Pentester Web -</p>
+                    </article>
+                </section>
+            </article>
+        </section>
+
+    </article>
+  </section>
+
   <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
@@ -364,77 +532,81 @@
   </div>
 
   <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-            <div class="container py-5">
-                <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                    <h5 class="fw-bold text-primary text-uppercase">Contactez-nous</h5>
-                    <h1 class="mb-0">Si vous avez des questions, n'hésitez pas à nous contacter</h1>
-                </div>
-                <div class="row g-5 mb-5">
-                    <div class="col-lg-4">
-                        <div id="contactNous" class="d-flex align-items-center wow fadeIn" data-wow-delay="0.1s">
-                            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
-                                <i class="fa fa-phone-alt text-white"></i>
-                            </div>
-                            <div class="ps-4">
-                                <h5 class="mb-2">Appeler</h5>
-                                <h4 class="text-primary mb-0">+33 755672680</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="d-flex align-items-center wow fadeIn" data-wow-delay="0.4s">
-                            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
-                                <i class="fa fa-envelope-open text-white"></i>
-                            </div>
-                            <div class="ps-4">
-                                <h5 class="mb-2">Email</h5>
-                                <h4 class="text-primary mb-0">aseck@bn2consulting.fr</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="d-flex align-items-center wow fadeIn" data-wow-delay="0.8s">
-                            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
-                                <i class="fa fa-map-marker-alt text-white"></i>
-                            </div>
-                            <div class="ps-4">
-                                <h5 class="mb-2">Notre Bureau</h5>
-                                <h4 class="text-primary mb-0">264 Av. Victor Hugo,94120 Fontenay-sous-Bois, France</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row g-5">
-                    <div class="col-lg-6 wow slideInUp" data-wow-delay="0.3s">
-                        <form >
-                            <div class="row g-3">
-                                <div class="col-md-6">
-                                    <input id="names" type="text" name="name" class="form-control border-0 bg-light px-4" placeholder="Votre noms" style="height: 55px;" required>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="emails" type="email" name="email" class="form-control border-0 bg-light px-4" placeholder="Votre Email" style="height: 55px;" required>
-                                </div>
-                                <div class="col-12">
-                                    <input type="text" name="subject" class="form-control border-0 bg-light px-4" placeholder="Objet" style="height: 55px;" required>
-                                </div>
-                                <div class="col-12">
-                                    <textarea name="message" class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message" required></textarea>
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100 py-3" type="submit">Envoyer Message</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-lg-6 wow slideInUp" data-wow-delay="0.6s">
-                        <iframe class="position-relative rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9607012406086!2d2.4759105769010974!3d48.85895977133227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e612a21858c049%3A0xeca1d8079a521141!2s264%20Av.%20Victor%20Hugo%2C%2094120%20Fontenay-sous-Bois!5e0!3m2!1sfr!2sfr!4v1723805018156!5m2!1sfr!2sfr"
-                            frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
-                            tabindex="0"></iframe>
-                    </div>
-                </div>
+    <div class="container py-5">
+      <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+        <h5 class="fw-bold text-primary text-uppercase">Contactez-nous</h5>
+        <h1 class="mb-0">Si vous avez des questions, n'hésitez pas à nous contacter</h1>
+      </div>
+      <div class="row g-5 mb-5">
+        <div class="col-lg-4">
+          <div id="contactNous" class="d-flex align-items-center wow fadeIn" data-wow-delay="0.1s">
+            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+              <i class="fa fa-phone-alt text-white"></i>
             </div>
+            <div class="ps-4">
+              <h5 class="mb-2">Appeler</h5>
+              <h4 class="text-primary mb-0">+33 755672680</h4>
+            </div>
+          </div>
         </div>
+        <div class="col-lg-4">
+          <div class="d-flex align-items-center wow fadeIn" data-wow-delay="0.4s">
+            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+              <i class="fa fa-envelope-open text-white"></i>
+            </div>
+            <div class="ps-4">
+              <h5 class="mb-2">Email</h5>
+              <h4 class="text-primary mb-0">aseck@bn2consulting.fr</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="d-flex align-items-center wow fadeIn" data-wow-delay="0.8s">
+            <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+              <i class="fa fa-map-marker-alt text-white"></i>
+            </div>
+            <div class="ps-4">
+              <h5 class="mb-2">Notre Bureau</h5>
+              <h4 class="text-primary mb-0">264 Av. Victor Hugo, 94120 Fontenay-sous-Bois, France</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row g-5">
+      <div class="col-lg-6 wow slideInUp" data-wow-delay="0.3s">
+        <form @submit.prevent="sendEmail">
+          <div class="row g-3">
+            <div class="col-md-6">
+              <input v-model="name" type="text" class="form-control border-0 bg-light px-4" placeholder="Votre nom" required style="height: 55px;">
+            </div>
+            <div class="col-md-6">
+              <input v-model="email" type="email" class="form-control border-0 bg-light px-4" placeholder="Votre Email" required style="height: 55px;">
+            </div>
+            <div class="col-12">
+              <input v-model="subject" type="text" class="form-control border-0 bg-light px-4" placeholder="Objet" required style="height: 55px;">
+            </div>
+            <div class="col-12">
+              <textarea v-model="message" class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message" required></textarea>
+            </div>
+            <div class="col-12">
+              <button class="btn btn-primary w-100 py-3" type="submit">Envoyer Message</button>
+            </div>
+            <div class="col-12 text-center">
+              <p v-if="successMessage" class="text-success mt-3">{{ successMessage }}</p>
+              <p v-if="errorMessage" class="text-danger mt-3">{{ errorMessage }}</p>
+            </div>
+          </div>
+        </form>
+        </div>
+        <div class="col-lg-6 wow slideInUp" data-wow-delay="0.6s">
+          <iframe class="position-relative rounded w-100 h-100"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9607012406086!2d2.4759105769010974!3d48.85895977133227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e612a21858c049%3A0xeca1d8079a521141!2s264%20Av.%20Victor%20Hugo%2C%2094120%20Fontenay-sous-Bois!5e0!3m2!1sfr!2sfr!4v1723805018156!5m2!1sfr!2sfr"
+            frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 <style scoped>
   .carScroll {
